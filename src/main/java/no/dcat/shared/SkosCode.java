1c901fbfe00eaa781767224be2034b4c676959ac
Alexander Zakharov <alexander.zakharov@capgemini.com>
2020-03-27 12:56:59 +0100

    fix: code imporvements

--- src/main/java/no/dcat/shared/SkosCode.java
+++ src/main/java/no/dcat/shared/SkosCode.java
@@ -1,39 +1,19 @@
 package no.dcat.shared;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.ToString;
+import lombok.NoArgsConstructor;
 
-import java.util.HashMap;
 import java.util.Map;
 
-/**
- * Created by bjg on 24.02.2017.
- * Model class to represent code  values
- */
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class SkosCode {
     private String uri;
     private String code;
-    private Map<String, String> prefLabel = new HashMap<>();
-
-    public SkosCode(String uri, String code, Map<String, String> prefLabel) {
-        this.uri = uri;
-        this.code = code;
-        this.prefLabel = prefLabel;
-    }
-
-    public SkosCode() {
-
-    }
-
-    @Override
-    public String toString() {
-        return "SkosCode{" +
-                "uri='" + uri + '\'' +
-                ", code='" + code + '\'' +
-                ", prefLabel=" + prefLabel +
-                '}';
-    }
+    private String name;
+    private Map<String, String> prefLabel;
 }
