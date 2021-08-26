6e57dda19e77009626930da7d18231deb9084740
Jeff Reiffers <jeffreiffers@gmail.com>
2021-08-26 12:20:30 +0200

    chore: no contextpath when using test profile

--- src/test/java/no/fdk/referencedata/eu/datatheme/DataThemeControllerIntegrationTest.java
+++ src/test/java/no/fdk/referencedata/eu/datatheme/DataThemeControllerIntegrationTest.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -16,6 +17,7 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
         properties = "scheduling.enabled=false")
+@ActiveProfiles("test")
 public class DataThemeControllerIntegrationTest extends AbstractMongoDbContainerTest {
 
     @LocalServerPort
