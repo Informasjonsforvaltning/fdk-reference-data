c610b83ff3f6b836a594efc55d54b554cd0c8a9d
Jeff Reiffers <jeffreiffers@gmail.com>
2021-08-26 09:44:41 +0200

    chore: restructuring iana source handling

--- src/main/java/no/fdk/referencedata/iana/mediatype/MediaTypeHarvester.java
+++ src/main/java/no/fdk/referencedata/iana/mediatype/MediaTypeHarvester.java
@@ -1,11 +1,9 @@
 package no.fdk.referencedata.iana.mediatype;
 
 import lombok.extern.slf4j.Slf4j;
-import no.fdk.referencedata.iana.mediatype.MediaType;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVRecord;
-import org.springframework.core.io.Resource;
 import org.springframework.core.io.UrlResource;
 import org.springframework.data.util.Pair;
 import org.springframework.stereotype.Component;
@@ -17,7 +15,6 @@ import java.net.MalformedURLException;
 import java.nio.charset.Charset;
 import java.util.List;
 import java.util.Objects;
-import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Predicate;
 
 import static java.lang.String.format;
@@ -42,51 +39,47 @@ public class MediaTypeHarvester implements IanaHarvester {
 
     public Flux<MediaType> harvest() {
         log.info("Starting harvest of IANA media types");
-
-        AtomicInteger count = new AtomicInteger();
-
         return getSources()
-            .flatMap(this::harvestMediaTypeResource)
-            .doOnNext(MediaType -> count.getAndIncrement())
-            .doFinally(signal -> log.info("Successfully harvested {} IANA media types", count.get()));
+            .flatMap(s -> harvestMediaTypeResource(s));
     }
 
-    public Flux<Resource> getSources()  {
+    public Flux<IanaSource> getSources()  {
         return registries
                 .map(s -> {
                     String url = format("%s/%s.csv", BASE_URI, s);
                     try {
-                        return new UrlResource(url);
+                        return new IanaSource(s, new UrlResource(url));
                     } catch (MalformedURLException e) {
                         log.error("Invalid IANA media type registry URL: {}", url, e);
                         return null;
                     }
                 })
-                .filter(Objects::nonNull)
-                .cast(Resource.class);
+                .filter(Objects::nonNull);
     }
 
-    private Flux<MediaType> harvestMediaTypeResource(Resource resource) {
-        return Mono.justOrEmpty(resource)
-            .flatMapMany(this::extractMediaTypeRegistryPairs)
+    private Flux<MediaType> harvestMediaTypeResource(IanaSource ianaSource) {
+        return Mono.justOrEmpty(ianaSource)
+            .flatMapMany(r -> extractMediaTypeRegistryPairs(r))
             .map(pair -> buildMediaType(pair.getFirst(), pair.getSecond()));
     }
 
-    private Flux<Pair<String, String>> extractMediaTypeRegistryPairs(Resource resource) {
+    private Flux<Pair<String, String>> extractMediaTypeRegistryPairs(IanaSource ianaSource) {
         try {
             CSVFormat format = CSVFormat.DEFAULT
                 .withFirstRecordAsHeader()
                 .withSkipHeaderRecord();
             Charset charset = Charset.defaultCharset();
 
-            List<CSVRecord> records = CSVParser.parse(resource.getInputStream(),
+            List<CSVRecord> records = CSVParser.parse(ianaSource.getResource().getInputStream(),
                     charset, format).getRecords();
 
             return Flux.fromIterable(records)
-                .map(record -> Pair.of(record.get(0).trim(), record.get(1).trim()))
+                .map(record -> Pair.of(record.get(0).trim(), record.get(1).isBlank() ?
+                        format("%s/%s", ianaSource.getType(), record.get(0).trim()) :
+                        record.get(1).trim()))
                 .filter(Predicate.not(pair -> pair.getFirst().isEmpty() || pair.getSecond().isEmpty()));
         } catch (IOException e) {
-            log.error("Failed to extract media type registry records from: {}", resource.getFilename(), e);
+            log.error("Failed to extract media type registry records from: {}", ianaSource.getResource().getFilename(), e);
             return Flux.error(e);
         }
     }
