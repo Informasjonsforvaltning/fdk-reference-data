7ee6a8283291ea90fd25d286b3e22c65bf4bd3fc
Jeff Reiffers <jeffreiffers@gmail.com>
2021-09-14 16:14:44 +0200

    chore: los controller integration tests

---
+++ src/test/java/no/fdk/referencedata/los/LosControllerIntegrationTest.java
@@ -0,0 +1,62 @@
+package no.fdk.referencedata.los;
+
+import no.fdk.referencedata.i18n.Language;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.net.URI;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
+        properties = {
+                "scheduling.enabled=false",
+        })
+@ActiveProfiles("test")
+public class LosControllerIntegrationTest {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void test_if_get_all_los_nodes_returns_valid_response() {
+        LosNodes losNodes =
+                this.restTemplate.getForObject("http://localhost:" + port + "/los/themes-and-words", LosNodes.class);
+
+        assertEquals(519, losNodes.getLosNodes().size());
+
+        final LosNode first = losNodes.getLosNodes().get(0);
+        assertEquals("https://psi.norge.no/los/ord/abort", first.getUri());
+        assertEquals("Abort", first.getName().get(Language.NORWEGIAN_BOKMAAL.code()));
+        assertFalse(first.isTheme());
+        assertEquals(List.of("helse-og-omsorg/svangerskap/abort"), first.getLosPaths());
+        assertEquals(List.of(URI.create("https://psi.norge.no/los/tema/svangerskap")), first.getParents());
+        assertEquals(List.of("Svangerskapsavbrudd", "Svangerskapsavbrot"), first.getSynonyms());
+    }
+
+    @Test
+    public void test_if_get_los_nodes_by_uri_returns_valid_response() {
+        LosNodes losNodes =
+                this.restTemplate.getForObject("http://localhost:" + port + "/los/themes-and-words?uris={uris}",
+                        LosNodes.class, Map.of("uris", "https://psi.norge.no/los/ord/abort"));
+
+        assertEquals(1, losNodes.getLosNodes().size());
+        final LosNode first = losNodes.getLosNodes().get(0);
+        assertEquals("https://psi.norge.no/los/ord/abort", first.getUri());
+        assertEquals("Abort", first.getName().get(Language.NORWEGIAN_BOKMAAL.code()));
+        assertFalse(first.isTheme());
+        assertEquals(List.of("helse-og-omsorg/svangerskap/abort"), first.getLosPaths());
+        assertEquals(List.of(URI.create("https://psi.norge.no/los/tema/svangerskap")), first.getParents());
+        assertEquals(List.of("Svangerskapsavbrudd", "Svangerskapsavbrot"), first.getSynonyms());
+    }
+}
