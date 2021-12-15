b6c61540e453c3794c5aed8098240108f6ee950f
Jeff Reiffers <jeffreiffers@gmail.com>
2021-12-15 10:12:52 +0100

    chore: remove log4j dependencies

--- src/main/java/no/fdk/referencedata/security/WebSecurityConfig.java
+++ src/main/java/no/fdk/referencedata/security/WebSecurityConfig.java
@@ -1,24 +1,16 @@
 package no.fdk.referencedata.security;
 
+import com.google.common.base.Strings;
 import no.fdk.referencedata.ApplicationSettings;
-import org.apache.logging.log4j.util.Strings;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.web.cors.CorsConfiguration;
 
 @Configuration
@@ -38,7 +30,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         final APIKeyAuthFilter filter = new APIKeyAuthFilter("X-API-KEY");
         filter.setAuthenticationManager(authentication -> {
             String principal = (String) authentication.getPrincipal();
-            if (Strings.isBlank(principal) || !applicationSettings.getApiKey().equals(principal)) {
+            if (Strings.isNullOrEmpty(principal) || !applicationSettings.getApiKey().equals(principal)) {
                 throw new BadCredentialsException("The API key was not found or not the expected value.");
             }
             authentication.setAuthenticated(true);
