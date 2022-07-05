8f572722f2224ac18274733ba769df502b987d6c
NilsOveTen <github.nils.ove@tendenes.com>
2022-07-05 13:32:59 +0200

    feat: add linguistic system graphql

---
+++ src/main/java/no/fdk/referencedata/graphql/query/LinguisticSystemQuery.java
@@ -0,0 +1,32 @@
+package no.fdk.referencedata.graphql.query;
+
+import com.coxautodev.graphql.tools.GraphQLQueryResolver;
+import no.fdk.referencedata.linguisticsystem.LinguisticSystem;
+import no.fdk.referencedata.linguisticsystem.LinguisticSystemService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+public class LinguisticSystemQuery implements GraphQLQueryResolver {
+
+    private final LinguisticSystemService linguisticSystemService;
+
+    @Autowired
+    public LinguisticSystemQuery(LinguisticSystemService linguisticSystemService) {
+        this.linguisticSystemService = linguisticSystemService;
+    }
+
+    public List<LinguisticSystem> getLinguisticSystems() {
+        return linguisticSystemService.getAll().stream()
+                .sorted(Comparator.comparing(LinguisticSystem::getUri))
+                .collect(Collectors.toList());
+    }
+
+    public LinguisticSystem getLinguisticSystemByCode(String code) {
+        return linguisticSystemService.getByCode(code).orElse(null);
+    }
+}
