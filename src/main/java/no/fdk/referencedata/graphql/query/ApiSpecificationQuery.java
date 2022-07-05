c4e2464c1aaabec0286271cfbf628f2e4e834b99
NilsOveTen <github.nils.ove@tendenes.com>
2022-07-05 10:26:38 +0200

    feat: add api specification graphql

---
+++ src/main/java/no/fdk/referencedata/graphql/query/ApiSpecificationQuery.java
@@ -0,0 +1,32 @@
+package no.fdk.referencedata.graphql.query;
+
+import com.coxautodev.graphql.tools.GraphQLQueryResolver;
+import no.fdk.referencedata.apispecification.ApiSpecification;
+import no.fdk.referencedata.apispecification.ApiSpecificationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+public class ApiSpecificationQuery implements GraphQLQueryResolver {
+
+    private final ApiSpecificationService apiSpecificationService;
+
+    @Autowired
+    public ApiSpecificationQuery(ApiSpecificationService apiSpecificationService) {
+        this.apiSpecificationService = apiSpecificationService;
+    }
+
+    public List<ApiSpecification> getApiSpecifications() {
+        return apiSpecificationService.getAll().stream()
+                .sorted(Comparator.comparing(ApiSpecification::getUri))
+                .collect(Collectors.toList());
+    }
+
+    public ApiSpecification getApiSpecificationByCode(String code) {
+        return apiSpecificationService.getByCode(code).orElse(null);
+    }
+}
