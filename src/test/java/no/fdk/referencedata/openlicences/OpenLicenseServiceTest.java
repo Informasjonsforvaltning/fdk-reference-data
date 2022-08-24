c4579f718c9b9d0732107dbf160cd98438700154
NilsOveTen <github.nils.ove@tendenes.com>
2022-08-24 10:12:34 +0200

    feat: update open licenses with eu publication versions

--- src/test/java/no/fdk/referencedata/openlicences/OpenLicenseServiceTest.java
+++ src/test/java/no/fdk/referencedata/openlicences/OpenLicenseServiceTest.java
@@ -25,13 +25,12 @@ public class OpenLicenseServiceTest {
         service.importOpenLicenses();
 
         List<OpenLicense> licenses = service.getAll();
-        assertEquals(7, licenses.size());
+        assertEquals(3, licenses.size());
 
         OpenLicense first = licenses.get(0);
-        assertEquals("http://creativecommons.org/licenses/by/4.0/", first.getUri());
-        assertEquals("CC BY 4.0", first.getCode());
-        assertNull(first.getIsReplacedBy());
-        assertEquals("Creative Commons Attribution 4.0 International", first.getLabel().get(Language.ENGLISH.code()));
+        assertEquals("http://publications.europa.eu/resource/authority/licence/CC0", first.getUri());
+        assertEquals("CC0 1.0", first.getCode());
+        assertEquals("Creative Commons Universal Public Domain Dedication", first.getLabel().get(Language.ENGLISH.code()));
     }
 
     @Test
@@ -39,29 +38,14 @@ public class OpenLicenseServiceTest {
         OpenLicenseService service = new OpenLicenseService(new OpenLicenseImporter());
         service.importOpenLicenses();
 
-        Optional<OpenLicense> licenseOptional = service.getByCode("NLOD10");
+        Optional<OpenLicense> licenseOptional = service.getByCode("NLOD20");
 
         assertTrue(licenseOptional.isPresent());
 
         OpenLicense license = licenseOptional.get();
-        assertEquals("http://data.norge.no/nlod/no/1.0", license.getUri());
-        assertEquals("NLOD10", license.getCode());
-        assertEquals("http://data.norge.no/nlod/no/2.0", license.getIsReplacedBy());
-        assertEquals("Norwegian Licence for Open Government Data", license.getLabel().get(Language.ENGLISH.code()));
-    }
-
-    @Test
-    public void test_if_get_open_license_model_is_isomorphic_with_old_rdf() {
-        Model expected = ModelFactory.createDefaultModel();
-        expected.read(requireNonNull(OpenLicenseServiceTest.class.getClassLoader().getResource("open-licenses-skos.rdf")).toString());
-
-        OpenLicenseService service = new OpenLicenseService(new OpenLicenseImporter());
-        service.importOpenLicenses();
-
-        Model result = ModelFactory.createDefaultModel();
-        result.read(new StringReader(service.getRdf(RDFFormat.TURTLE)), "http://base.example.com", "TURTLE");
-
-        assertTrue(result.isIsomorphicWith(expected));
+        assertEquals("http://publications.europa.eu/resource/authority/licence/NLOD_2_0", license.getUri());
+        assertEquals("NLOD20", license.getCode());
+        assertEquals("Norsk lisens for offentlige data", license.getLabel().get(Language.NORWEGIAN.code()));
     }
 
 }
