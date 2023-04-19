f929e94aa042a468195e9ba41deb7b0091f16856
NilsOveTen <github.nils.ove@tendenes.com>
2023-04-19 10:42:31 +0200

    feat: add losPaths to rdf response

--- src/main/java/no/fdk/referencedata/rdf/RDFUtils.java
+++ src/main/java/no/fdk/referencedata/rdf/RDFUtils.java
@@ -1,10 +1,16 @@
 package no.fdk.referencedata.rdf;
 
 import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.RDFNode;
+import org.apache.jena.rdf.model.Resource;
 import org.apache.jena.riot.RDFDataMgr;
 import org.apache.jena.riot.RDFFormat;
+import org.apache.jena.vocabulary.SKOS;
+import org.springframework.data.util.Pair;
 
 import java.io.StringWriter;
+import java.util.Collection;
+import java.util.List;
 
 public class RDFUtils {
 
@@ -12,6 +18,26 @@ public class RDFUtils {
         StringWriter stringWriter = new StringWriter();
         RDFDataMgr.write(stringWriter, m, rdfFormat) ;
         return stringWriter.toString();
+    }
+
+    private static String getKeywordFromURI(String uri) {
+        return uri.substring(uri.lastIndexOf('/') + 1);
+    }
+
+    public static List<String> generateThemePaths(Model m, Resource theme) {
+        String keyword = getKeywordFromURI(theme.getURI());
+        List<Resource> parents = m.listObjectsOfProperty(theme, SKOS.broader)
+                .mapWith(RDFNode::asResource)
+                .toList();
+        if (parents.isEmpty()) {
+            return List.of(keyword);
+        } else {
+            return parents.stream()
+                    .map(parent -> generateThemePaths(m, parent))
+                    .flatMap(Collection::stream)
+                    .map(parentPath -> parentPath + "/" + keyword)
+                    .toList();
+        }
     }
 
 }
