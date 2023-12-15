52092d47086ce80de38ce8b286f0ea4b9ca41afb
NilsOveTen <github.nils.ove@tendenes.com>
2023-12-15 07:37:42 +0100

    feat: rdf-endpoint for api status

--- src/test/java/no/fdk/referencedata/apistatus/ApiStatusServiceTest.java
+++ src/test/java/no/fdk/referencedata/apistatus/ApiStatusServiceTest.java
@@ -1,26 +1,25 @@
 package no.fdk.referencedata.apistatus;
 
 import no.fdk.referencedata.i18n.Language;
-import org.apache.jena.rdf.model.Model;
-import org.apache.jena.rdf.model.ModelFactory;
-import org.apache.jena.riot.RDFFormat;
+import no.fdk.referencedata.rdf.RDFSourceRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.test.context.ActiveProfiles;
 
-import java.io.StringReader;
 import java.util.List;
 import java.util.Optional;
 
-import static java.util.Objects.requireNonNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.mock;
 
 @ActiveProfiles("test")
 public class ApiStatusServiceTest {
 
+    private final RDFSourceRepository rdfSourceRepository = mock(RDFSourceRepository.class);
+
     @Test
     public void test_if_get_all_returns_all_api_statuses() {
-        ApiStatusService service = new ApiStatusService(new ApiStatusImporter());
+        ApiStatusService service = new ApiStatusService(new ApiStatusImporter(), rdfSourceRepository);
         service.importApiStatuses();
 
         List<ApiStatus> statuses = service.getAll();
@@ -34,7 +33,7 @@ public class ApiStatusServiceTest {
 
     @Test
     public void test_if_get_api_status_by_code_returns_correct_api_status() {
-        ApiStatusService service = new ApiStatusService(new ApiStatusImporter());
+        ApiStatusService service = new ApiStatusService(new ApiStatusImporter(), rdfSourceRepository);
         service.importApiStatuses();
 
         Optional<ApiStatus> statusOptional = service.getByCode("EXPERIMENTAL");
@@ -45,20 +44,6 @@ public class ApiStatusServiceTest {
         assertEquals("http://fellesdatakatalog.brreg.no/reference-data/codes/apistastus/nonproduction", apiStatus.getUri());
         assertEquals("EXPERIMENTAL", apiStatus.getCode());
         assertEquals("Experimental", apiStatus.getLabel().get(Language.ENGLISH.code()));
-    }
-
-    @Test
-    public void test_if_get_api_status_model_is_isomorphic_with_old_rdf() {
-        Model expected = ModelFactory.createDefaultModel();
-        expected.read(requireNonNull(ApiStatusServiceTest.class.getClassLoader().getResource("api-status-skos.ttl")).toString());
-
-        ApiStatusService service = new ApiStatusService(new ApiStatusImporter());
-        service.importApiStatuses();
-
-        Model result = ModelFactory.createDefaultModel();
-        result.read(new StringReader(service.getRdf(RDFFormat.TURTLE)), "http://base.example.com", "TURTLE");
-
-        assertTrue(result.isIsomorphicWith(expected));
     }
 
 }
