61388baeb6a54d2f8465ad0a486a43e36f189044
Nils Ove Tendenes <nils.ove@tendenes.com>
2025-05-02 07:33:57 +0200

    chore: switch mongodb host

--- src/main/java/no/fdk/referencedata/rdf/RDFSource.java
+++ src/main/java/no/fdk/referencedata/rdf/RDFSource.java
@@ -2,8 +2,10 @@ package no.fdk.referencedata.rdf;
 
 import lombok.Data;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
+@Document(collection = "rdfSources")
 public class RDFSource {
     @Id
     String id;
